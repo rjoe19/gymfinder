01a963920e63e68c46e3c3ac473ab17fadfd79f2
Richard Joe <rjoe19@hotmail.com>
2016-02-17 19:00:27 +1300

    whatever

--- public/app.js
+++ public/app.js
@@ -1,9 +1,46 @@
+// var gyms = [
+//   {"Name":"Les Mills Extreme", "Address": "52-70 Taranaki St", "Ph":"04-384 8898", "CasualPrice": "$15.00"},
+//   {"Name":"Snap Fitness", "Address": "49-61 Tory St", "Ph":"04-802 5920", "CasualPrice": "$15.00"},
+//   {"Name":"Jetts Fitness", "Address": "49 Cuba St", "Ph":"04-212 4909", "CasualPrice": "$15.00"},
+//   {"Name":"City Fitness Thorndon", "Address": "75 Thorndon Quay", "Ph":"04-499 4488", "CasualPrice": "$15.00"},
+//   {"Name":"Club Kelburn", "Address": "65 Salamanca Rd", "Ph":"04-472 9299", "CasualPrice": "$15.00"}
+// ]
+//
+//
+//
+// var names = gyms.map(function(gym) {
+//   return gym.Name + ", "+ gym.Address + ", "+ gym.Ph + ", "+ gym.CasualPrice
+// })
+//
+// console.log(names)
+var fs = require('fs')
+
+var extractJson = fs.readFileSync(__dirname + './data/db.json', 'utf-8')
+console.log(extractJson)
+
+
+
+
 
 $( document ).ready(function() {
 //wellington
   $("#button-wgtn").click(function(){
       $.get("/gym-wgtn", function(data, status){
-          alert("Data: " + data + "\nStatus: " + status);
+
+
+        //  $("#info-wgtn").html(JSON.stringify(data.Gyms.Wellington))
+
+        //pseudocode....
+        // 1) pull in db.json ---->fs.readFile
+        //2) extract info from db.json out of the object.....then finally out of array
+
+        // 3) display result of that inside a pretty div
+
+
+
+
+        console.log("wellington working", data)
+
       });
   });
 
@@ -11,7 +48,7 @@ $( document ).ready(function() {
 
   $("#button-akl").click(function(){
       $.get("/gym-akl", function(data, status){
-          alert("Data: " + data + "\nStatus: " + status);
+          console.log("auckland working")
       });
   });
 
@@ -21,7 +58,7 @@ $( document ).ready(function() {
   //christchurch
   $("#button-chch").click(function(){
       $.get("/gym-chch", function(data, status){
-          alert("Data: " + data + "\nStatus: " + status);
+          console.log("christchurch working")
       });
   });
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var fs = require('fs')
 
 // Define the port to run on
 app.set('port', 3000);
@@ -14,14 +15,49 @@ var server = app.listen(app.get('port'), function() {
   console.log('Magic happens on port ' + port);
 });
 
-app.get('/gym-chch', function (req, res) {
-  app.json(['les mills', 'city fitness', 'jetts'])
-})
 
+
+
+//serve get requests for gyms after button pressed
 app.get('/gym-wgtn', function (req, res) {
-  console.log('hit gym wgtn')
+    fs.readFile('./data/db.json', 'utf8', function (err, data) {
+      console.log("got gym wgtn")
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
 })
 
 app.get('/gym-akl', function (req, res) {
-  console.log('hit gym akl')
+    fs.readFile('./data/db.json', 'utf8', function (err, data) {
+      console.log("got gym akl")
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
 })
+
+
+
+
+app.get('/gym-chch', function (req, res) {
+    fs.readFile('./data/db.json', 'utf8', function (err, data) {
+      console.log("got gym chch")
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
+})
+
+
+
+
+//
+// app.get('/gym-chch', function (req, res) {
+//   app.json(['les mills', 'city fitness', 'jetts'])
+// })
+//
+// app.get('/gym-wgtn', function (req, res) {
+//   console.log('hit gym wgtn')
+// })
+//
+// app.get('/gym-akl', function (req, res) {
+//   console.log('hit gym akl')
+// })
