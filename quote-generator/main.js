b1ccf87d3815fa70542f49d5dbadbb5fecf6fc7d
Richard Joe <rjoe19@hotmail.com>
2016-02-19 18:35:14 +1300

    Add quotes

---
+++ quote-generator/main.js
@@ -0,0 +1,18 @@
+var quoteText = {
+  "motivationalQuotes": [
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"},
+    {"quote": "No man is more unhappy than he who never faces adversity.  For he is not permitted to prove himself"}
+
+
+
+]
+}
